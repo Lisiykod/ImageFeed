9fad2d1f6ace636e6ae2cb0dcb45caf6b3172ece
Olga Trofimova <k12to1@yandex.ru>
2024-09-03 16:55:10 +0300

    добавлен новый контроллер и его базовая верстка

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Olga Trofimova on 03.09.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet private var exitImage: UIButton!
+    @IBOutlet private var mainNameLabel: UILabel!
+    @IBOutlet private var userpick: UIImageView!
+    @IBOutlet private var statusLabel: UILabel!
+    @IBOutlet private var logoLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func exitProfile(_ sender: Any) {
+    }
+    
+    
+}
