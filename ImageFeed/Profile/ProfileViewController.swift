991b91e55748907bca1f8dfd38a00ba01c724447
Olga Trofimova <k12to1@yandex.ru>
2024-09-10 01:22:49 +0300

    удалено лишнее, доработана инициализация вью

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -9,162 +9,92 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
-    private var exitButton: UIButton?
-    private var mainNameLabel: UILabel?
-    private var userpick: UIImageView?
-    private var statusLabel: UILabel?
-    private var logoLabel: UILabel?
-    private var noFavoritePhotoImage: UIImageView?
+    private let exitButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "exit_image"), for: .normal)
+        button.addTarget(self, action: #selector(didTapExitButton), for: .touchUpInside)
+        button.tintColor = .ypRed
+        return button
+    }()
     
-    private var favoritesLabel: UILabel?
-    // лейбл, который появится, когда настроим таблицу с избранным
-    private var notificationLabel: UILabel?
+    private var userPick: UIImageView = {
+        let userImage = UIImage(named: "userpick_photo")
+        let image = UIImageView(image: userImage)
+        return image
+    }()
+    
+    private let mainNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 23, weight: .bold)
+        label.textColor = .ypWhite
+        label.text = "Екатерина Новикова"
+        return label
+    }()
+    
+    private var logoLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        label.textColor = .ypGray
+        label.text = "@ekaterina_nov"
+        return label
+    }()
+    
+    private var statusLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        label.textColor = .ypWhite
+        label.text = "Hello, world!"
+        return label
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        initViews()
-        setupFonts()
-        setupLabels()
         addViewsToSuperView()
         setupConstraints()
     }
     
-    // метод для инициализации вьюшек
-    private func initViews() {
-        exitButton = UIButton.systemButton(with: UIImage(named: "exit_image") ?? UIImage(), target: self, action: #selector(didTapExitButton))
-        exitButton?.tintColor = .ypRed
-        mainNameLabel = UILabel()
-        statusLabel = UILabel()
-        logoLabel = UILabel()
-        let userImage = UIImage(named: "userpick_photo")
-        userpick = UIImageView(image: userImage)
-        favoritesLabel = UILabel()
-        let noPhoto = UIImage(named: "no_photo_image")
-        noFavoritePhotoImage = UIImageView(image: noPhoto)
-    }
-    
     // метод, в котором всех добавляем в иерархию
     private func addViewsToSuperView() {
-        guard
-            let exitButton = exitButton,
-            let mainNameLabel = mainNameLabel,
-            let statusLabel = statusLabel,
-            let logoLabel = logoLabel,
-            let userpick = userpick,
-            let favoritesLabel = favoritesLabel,
-            let noPhotoImage = noFavoritePhotoImage
-        else {
-            return
+        let viewsArray: [UIView] = [exitButton, userPick, mainNameLabel, logoLabel, statusLabel]
+        viewsArray.forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
         }
-        view.addSubview(exitButton)
-        view.addSubview(mainNameLabel)
-        view.addSubview(statusLabel)
-        view.addSubview(logoLabel)
-        view.addSubview(userpick)
-        view.addSubview(favoritesLabel)
-        view.addSubview(noPhotoImage)
-    }
-    
-    // метод для настройки шрифтов
-    private func setupFonts() {
-        mainNameLabel?.font = UIFont.systemFont(ofSize: 23, weight: .bold)
-        mainNameLabel?.textColor = .ypWhite
-        logoLabel?.font = UIFont.systemFont(ofSize: 13, weight: .regular)
-        logoLabel?.textColor = .ypGray
-        statusLabel?.font = UIFont.systemFont(ofSize: 13, weight: .regular)
-        statusLabel?.textColor = .ypWhite
-        favoritesLabel?.font = UIFont.systemFont(ofSize: 23, weight: .bold)
-        favoritesLabel?.textColor = .ypWhite
-    }
-    
-    // метод для настройки текста в лейблах
-    private func setupLabels() {
-        mainNameLabel?.text = "Екатерина Новикова"
-        logoLabel?.text = "@ekaterina_nov"
-        statusLabel?.text = "Hello, world!"
-        favoritesLabel?.text = "Избранное"
     }
     
     // метод для настройки автолейаута
     private func setupConstraints() {
         setUserPickAndExitButtonConstraints()
         setupLabelsConstraints()
-        setupNoFavoritePhotoImageConstraints()
     }
     
     // метод для настройки констрейнтов изображения юзера и кнопки выход
     private func setUserPickAndExitButtonConstraints() {
-        guard
-            let exitButton = exitButton,
-            let userpick = userpick
-        else {
-            return
-        }
-        
-        exitButton.translatesAutoresizingMaskIntoConstraints = false
-        userpick.translatesAutoresizingMaskIntoConstraints = false
-        
         NSLayoutConstraint.activate([
-            userpick.widthAnchor.constraint(equalToConstant: 70),
-            userpick.heightAnchor.constraint(equalToConstant: 70),
-            userpick.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            userpick.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            userPick.widthAnchor.constraint(equalToConstant: 70),
+            userPick.heightAnchor.constraint(equalToConstant: 70),
+            userPick.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            userPick.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
             exitButton.widthAnchor.constraint(equalToConstant: 44),
             exitButton.heightAnchor.constraint(equalToConstant: 44),
-            exitButton.centerYAnchor.constraint(equalTo: userpick.centerYAnchor),
+            exitButton.centerYAnchor.constraint(equalTo: userPick.centerYAnchor),
             view.trailingAnchor.constraint(equalTo: exitButton.trailingAnchor, constant: 12)
         ])
     }
     
     // метод для настройки констрейнтов для лейблов
     private func setupLabelsConstraints() {
-        guard
-            let mainNameLabel = mainNameLabel,
-            let statusLabel = statusLabel,
-            let logoLabel = logoLabel,
-            let userpick = userpick,
-            let favoritesLabel = favoritesLabel
-        else {
-            return
-        }
-        
-        mainNameLabel.translatesAutoresizingMaskIntoConstraints = false
-        statusLabel.translatesAutoresizingMaskIntoConstraints = false
-        logoLabel.translatesAutoresizingMaskIntoConstraints = false
-        favoritesLabel.translatesAutoresizingMaskIntoConstraints = false
-        
         NSLayoutConstraint.activate([
-            mainNameLabel.leadingAnchor.constraint(equalTo: userpick.leadingAnchor),
-            mainNameLabel.topAnchor.constraint(equalTo: userpick.bottomAnchor, constant: 8),
+            mainNameLabel.leadingAnchor.constraint(equalTo: userPick.leadingAnchor),
+            mainNameLabel.topAnchor.constraint(equalTo: userPick.bottomAnchor, constant: 8),
             view.trailingAnchor.constraint(greaterThanOrEqualTo: mainNameLabel.trailingAnchor, constant: 124),
             logoLabel.leadingAnchor.constraint(equalTo: mainNameLabel.leadingAnchor),
             logoLabel.topAnchor.constraint(equalTo: mainNameLabel.bottomAnchor, constant: 8),
             statusLabel.leadingAnchor.constraint(equalTo: mainNameLabel.leadingAnchor),
-            statusLabel.topAnchor.constraint(equalTo: logoLabel.bottomAnchor, constant: 8),
-            favoritesLabel.leadingAnchor.constraint(equalTo: mainNameLabel.leadingAnchor),
-            favoritesLabel.topAnchor.constraint(equalTo: statusLabel.bottomAnchor, constant: 24)
+            statusLabel.topAnchor.constraint(equalTo: logoLabel.bottomAnchor, constant: 8)
         ])
     }
     
-    // метод для настройки констрейнтов картинки "нет избранных"
-    private func setupNoFavoritePhotoImageConstraints() {
-        
-        guard
-            let favoritesLabel = favoritesLabel,
-            let noPhotoImage = noFavoritePhotoImage
-        else {
-            return
-        }
-        
-        noPhotoImage.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            noPhotoImage.widthAnchor.constraint(equalToConstant: 101),
-            noPhotoImage.heightAnchor.constraint(equalToConstant: 115),
-            noPhotoImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            noPhotoImage.topAnchor.constraint(lessThanOrEqualTo: favoritesLabel.bottomAnchor, constant: 103)
-        ])
-    }
     // метод для кнопки выход
     @objc
     private func didTapExitButton() {
